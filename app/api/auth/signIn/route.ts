6a818d5d5dbb0c413357eb4d870b857d9d034e67
CollinsEzeokeke <collinsmecosy@gmail.com>
2024-11-25 09:05:36 +0100

    added a form for authentcation

--- app/api/auth/signIn/route.ts
+++ app/api/auth/signIn/route.ts
@@ -1,22 +1,23 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { authClient } from "@/lib/auth-Client";
 
 export async function POST(req: Request) {
-    try {const { email, password } = await req.json()
+  try {
+    const { email, password } = await req.json()
     const { data, error } = await authClient.signIn.email({
-        email: email,
-        password: password,
-        callbackURL: "/",
-      });
-      if (data){
-      return NextResponse.json({data})
-      }
-      return NextResponse.json({error})
-      } catch (error) {
-        NextResponse.json(
-            {message: "this was an error" + error},
-            {status: 400}
-        )
-      }
+      email: email,
+      password: password,
+      callbackURL: "/",
+    });
+    if (data) {
+      return NextResponse.json({ data })
+    }
+    return NextResponse.json({ error })
+  } catch (error) {
+    NextResponse.json(
+      { message: "this was an error" + error },
+      { status: 400 }
+    )
+  }
 }
 
